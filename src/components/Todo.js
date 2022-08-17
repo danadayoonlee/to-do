5eefbd17d6ae7a28f225357552a375ed66b94ce0
Dayoon Lee <danadayoonlee@gmail.com>
2022-08-17 18:47:44 -0400

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
-import { RiCloseCircleLine } from 'react-icons/ri'; // icon
-import { TiEdit } from 'react-icons/ti'; // icon
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
